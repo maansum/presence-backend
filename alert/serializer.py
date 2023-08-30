277d3e24972c5f27999dbb9e02b01d975648d7c6
unknown <sumandarji2479@gmail.com>
2023-08-30 15:32:34 +0545

    small change

--- alert/serializer.py
+++ alert/serializer.py
@@ -11,6 +11,21 @@ class AlertSerializer(serializers.ModelSerializer):
 
 
 class GetAlertSerializer(serializers.ModelSerializer):
+    groupname = serializers.SerializerMethodField()
+    sendername = serializers.SerializerMethodField()
+
     class Meta:
-        model= AlertModel
-        fields=['id','message','sender','send_at','read','group']
+        model = AlertModel
+        fields = ['id', 'message', 'sender', 'send_at', 'read', 'group', 'groupname', 'sendername']
+
+    def get_groupname(self, obj):
+        # Replace this with your logic to compute groupname
+        if obj.group:
+            return obj.group.name  # Assuming 'name' is a field in your GroupModel
+        return None
+
+    def get_sendername(self, obj):
+        # Replace this with your logic to compute sendername
+        if obj.sender:
+            return obj.sender.name  # Assuming 'name' is a field in your SenderModel
+        return None
